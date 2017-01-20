918bdf122c865a7465df0fea05bc36727145d30d
Alexis06 <brian.bobi@outlook.com>
2017-01-20 13:03:45 -0600

    Esta lista la pagina

--- controller/js/listener.js
+++ controller/js/listener.js
@@ -1,4 +1,1 @@
-function textoEs(){
-    document.getElementById('areaTexto').innerHTML = "Hola ayudamos a mejorar tu experiencia. Selecciona el idioma en el que deseas navegar.";
-}
-$('body').scrollspy({ target: '#navbar-contenido' })
+$('body').scrollspy({ target: '#navbar-contenido' });
